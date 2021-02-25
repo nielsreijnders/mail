a1a716b6d2e176faafa96908bacdbfb6cb6cd27f
Niels <niels.reijnders@edu-kw1c.nl>
2021-02-26 00:25:03 +0100

    add: fonts and colors

---
+++ src/styles/index.js
@@ -0,0 +1,22 @@
+export const desktopBreakpoint = '1024px';
+
+export const colors = {
+  black: '#000000',
+  white: '#fff',
+  lightGrey: '#FAFAFA',
+  mediumGrey: '#F2F2F2',
+  darkGrey: '#BEBBBB',
+  mailleGold: '#AE841F',
+  red: '#DB4155',
+  darkBlue: '#0F0D9C',
+  blue: '#2760FF',
+  green: '#319865',
+  orange: '#F09400',
+};
+
+export const fonts = {
+  regular: 'sans-serif',
+  title: 'Helvetica',
+};
+
+export const letterSpacing = (space) => `${(space / 1000) * 1}em`;
