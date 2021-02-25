e6c2783460eb59fb3bee259b133ee8c01371b799
Niels <niels.reijnders@edu-kw1c.nl>
2021-02-25 23:35:56 +0100

    fix: code formatter

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,37 +3,35 @@ import React from 'react';
 const style = {
   container: {
     width: '100%',
-    fontFamily: "'roboto_regular', 'calibri', Arial, sans-serif",
-    background: "#fff",
+    fontFamily: 'roboto_regular, calibri, Arial, sans-serif',
+    background: '#fff',
   },
   styledImage: {
     border: 0,
     height: 'auto',
     display: 'block',
-    width: "160px",
+    width: '160px',
     margin: '0 auto',
     padding: '30px',
-  }
+  },
 };
 
-const Header = ({children}) => {
-  return (
-    <table 
-      style={style.container}
-      >
-      <tbody>
-        <tr>
-          <a>
+const Header = () => (
+  <table
+    style={style.container}
+  >
+    <tbody>
+      <tr>
+        <a>
           <img
             alt="HEYO logo"
             src="http://cdn.mcauto-images-production.sendgrid.net/27c8c65ba7b3d635/e6814b23-0cb7-4b99-a2fc-319dc28a2747/640x160.png"
             style={style.styledImage}
-            />
-          </a>
-        </tr>
-      </tbody>
-    </table>
-  );
-};
+          />
+        </a>
+      </tr>
+    </tbody>
+  </table>
+);
 
 export default Header;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,25 +8,23 @@ const style = {
   },
 };
 
-const Layout = ({children}) => {
-  return (
-    <table 
-      cellpadding="0"
-      cellspacing="0"
-      border="0"
-      bgcolor="#F9F7F0"
-      width="768"
-      min-width="768px"
-      style={style.container}
-      >
-      <tbody>
-        <tr>
-          <Header />
-          {children}
-        </tr>
-      </tbody>
-    </table>
-  );
-};
+const Layout = ({ children }) => (
+  <table
+    cellPadding="0"
+    cellSpacing="0"
+    border="0"
+    bgcolor="#F9F7F0"
+    width="768"
+    min-width="768px"
+    style={style.container}
+  >
+    <tbody>
+      <tr>
+        <Header />
+        {children}
+      </tr>
+    </tbody>
+  </table>
+);
 
 export default Layout;
